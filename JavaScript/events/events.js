cb0d6a09aeec3496903ec030337e77c1b786f028
goelabhishek694 <goelabhishek694@gmail.com>
2023-03-23 21:21:59 +0530

    chnage event in select element

--- JavaScript/events/events.js
+++ JavaScript/events/events.js
@@ -1,3 +1,14 @@
 function myFunction(val) {
     console.log(val);
 }
+//onchnage event -> is an event attribute , we write itinside element. Whenever there is a change in value of element, onchnage vent occurs which in turn calls the function asociated with it. 
+
+const selectEle = document.getElementById("mySelect");
+console.log(selectEle);
+
+selectEle.addEventListener("change", cb);
+//if there is a change in my element , call cb
+function cb() {
+    console.log("Technology selected -> ", selectEle.value);
+}
+
