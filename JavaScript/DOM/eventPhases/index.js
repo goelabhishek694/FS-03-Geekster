91edaab4b88c8ad75549a80d5e991120aa8b854b
goelabhishek694 <goelabhishek694@gmail.com>
2023-03-29 21:24:40 +0530

    :fire: InterviewQues, stopping event propogation

--- JavaScript/DOM/eventPhases/index.js
+++ JavaScript/DOM/eventPhases/index.js
@@ -1,31 +1,45 @@
 const elements = document.querySelectorAll("*");
-console.log(elements);
-for (let ele of elements) {
-  //bubbling
-  ele.addEventListener("click", (e) => {
-    console.log(
-      "Bubbling -> ",
-      ele.tagName,
-      "Clicked on ->",
-      e.currentTarget,
-      "Originated from -> ",
-      e.target
-    );
-  });
+// console.log(elements);
+// for (let ele of elements) {
+//   //bubbling
+//   ele.addEventListener("click", (e) => {
+//     console.log(
+//       "Bubbling -> ",
+//       ele.tagName,
+//       "Clicked on ->",
+//       e.currentTarget,
+//       "Originated from -> ",
+//       e.target
+//     );
+//   });
+    
+//   //capturing
+//   ele.addEventListener(
+//     "click",
+//     (e) => {
+//       console.log(
+//         "Capturing -> ",
+//         ele.tagName,
+//         "Clicked on ->",
+//         e.currentTarget,
+//         "Originated from -> ",
+//         e.target
+//       );
+//     },
+//     true
+//   );
+// }
 
-  capturing
-  ele.addEventListener(
-    "click",
-    (e) => {
-      console.log(
-        "Capturing -> ",
-        ele.tagName,
-        "Clicked on ->",
-        e.currentTarget,
-        "Originated from -> ",
-        e.target
-      );
-    },
-    true
-  );
-}
+document.querySelector(".parent").addEventListener("click", function(e){
+    console.log(`Click event is fired on ${this.nodeName}1`)
+})
+
+document.querySelector(".child").addEventListener("click", function (e) {
+  // stopping event propogation
+  e.stopPropagation();
+  console.log(`Click event is fired on ${this.nodeName}2`);
+});
+
+
+
+
