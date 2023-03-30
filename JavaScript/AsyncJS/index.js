136b1851535073845a310df2e6bdb8664f2cb570
goelabhishek694 <goelabhishek694@gmail.com>
2023-03-30 22:03:14 +0530

    :fire: callback hell example

--- JavaScript/AsyncJS/index.js
+++ JavaScript/AsyncJS/index.js
@@ -4,25 +4,47 @@
 // console.log(1000);
 
 function getData(cb) {
-    setTimeout(() => {
-        const data = [10, 20, 30, 40, 50];
-        cb(data);
-    }, 5000);
-    for(let i=0;i<10;i++){
-        console.log(i);
+  setTimeout(() => {
+    const data = [10, 20, 30, 40, 50];
+    cb(data);
+  }, 5000);
+  for (let i = 0; i < 10; i++) {
+    console.log(i);
+  }
+}
+
+getData(cb);
+
+function cb(arr) {
+  console.log(arr);
+}
+
+//callback hell
+// dealing with multiple/nested callbacks
+//code is not readable, difficult to maintain.
+
+asyncFunction1(
+    function (err, result1) {
+        if (err) console.log(err);
+        else {
+            asyncFunction2(result1, 
+                function (err, result2) {
+                    if (err) console.log(err);
+                    else{
+                        asyncFunction3(result2,function(err,result3){
+                            if(err) console.log(err);
+                            else console.log("Pyramid of doom");
+                        })
+                    }
+                }
+            );
+        }
     }
-}
+);
 
-getData(cb)
+//promises 
 
-function cb(arr){
-    console.log(arr)
-}
 
-// addNum1((result1)=>{
-//     addNum2(result1,(result2)=>{
-//         addNum3(result2,(result3)=>{
-//             console.log(result3);
-//         })
-//     })
-// })
+
+
+
