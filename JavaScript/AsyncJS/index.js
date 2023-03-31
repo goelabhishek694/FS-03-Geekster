ae9df148543a512b8795c57c4f01d5a4f9145a2b
goelabhishek694 <goelabhishek694@gmail.com>
2023-03-31 21:07:22 +0530

    producer,consumer in promise

--- JavaScript/AsyncJS/index.js
+++ JavaScript/AsyncJS/index.js
@@ -3,57 +3,102 @@
 // setTimeout(() => console.log("200"), 3000);
 // console.log(1000);
 
-function getData(cb) {
-  setTimeout(() => {
-    const data = [10, 20, 30, 40, 50];
-    cb(data);
-  }, 5000);
-  for (let i = 0; i < 10; i++) {
-    console.log(i);
-  }
-}
+// function getData(cb) {
+//   setTimeout(() => {
+//     const data = [10, 20, 30, 40, 50];
+//     cb(data);
+//   }, 5000);
+//   for (let i = 0; i < 10; i++) {
+//     console.log(i);
+//   }
+// }
 
-getData(cb);
+// getData(cb);
 
-function cb(arr) {
-  console.log(arr);
-}
+// function cb(arr) {
+//   console.log(arr);
+// }
 
 //callback hell
 // dealing with multiple/nested callbacks
 //code is not readable, difficult to maintain.
 
-asyncFunction1(
-    function (err, result1) {
-        if (err) console.log(err);
-        else {
-            asyncFunction2(result1, 
-                function (err, result2) {
-                    if (err) console.log(err);
-                    else{
-                        asyncFunction3(result2,function(err,result3){
-                            if(err) console.log(err);
-                            else console.log("Pyramid of doom");
-                        })
-                    }
-                }
-            );
-        }
-    }
-);
+// asyncFunction1(
+//     function (err, result1) {
+//         if (err) console.log(err);
+//         else {
+//             asyncFunction2(result1, 
+//                 function (err, result2) {
+//                     if (err) console.log(err);
+//                     else{
+//                         asyncFunction3(result2,function(err,result3){
+//                             if(err) console.log(err);
+//                             else console.log("Pyramid of doom");
+//                         })
+//                     }
+//                 }
+//             );
+//         }
+//     }
+// );
 
 //promises 
 
 //creating a promise
                                      //executor
-let companyStatusPromise=new Promise(function(resolve,reject){
+// let promise=new Promise(function(resolve,reject){
   //contains api call->result
   // resolve,reject -> cb functions
   // either of resolve() or reject() is called
   //resolve(value)-> if job is finished succesfully with result "value"
   //reject(error) -> if an error occurs
-})
+// })
 //when new Promise is created , executor is called automatically
+
+let companyStatusPromise = new Promise(function (resolve, reject) { 
+  //i am sure i will get the value
+  setTimeout(() => resolve("hoorrayy"), 3000);
+  // setTimeout(() => reject(new Error("corrupted file")), 1000);
+})
+
+console.log(companyStatusPromise);
+
+//consumers -> then and catch
+
+// then
+                          
+// promise.then(
+//   function (result) {}, //will be called if promise is fulfilled
+//   function (err) {} //will be called if promise is rejected
+// );
+
+companyStatusPromise.then((result) => console.log(result), (error) => console.log(error))
+
+let isEvenPromise = new Promise(function (resolve, reject) {
+  function getVal() {
+    let val = Math.floor(Math.random() * 100);
+    // let val = 100;
+    if (val % 2 == 0) resolve(`Received even number ${val}`);
+    else reject(new Error(`received odd number, ${val}`));
+  }
+  setTimeout(getVal,4000);
+})
+
+// isEvenPromise.then(
+//   (result) => console.log(result),
+//   (err) => console.log(err)
+// );
+
+isEvenPromise.then(
+  (result) => console.log(result)
+).catch(err => console.log(err));
+
+
+
+
+
+
+
 
 
 
