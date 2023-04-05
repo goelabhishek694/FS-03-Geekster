d300943c6e70b882027df616d3e246170ac6324b
goelabhishek694 <goelabhishek694@gmail.com>
2023-04-05 22:04:46 +0530

    :fire: async await finsished

--- JavaScript/AsyncJS/asyncAwait.js
+++ JavaScript/AsyncJS/asyncAwait.js
@@ -7,43 +7,75 @@
 // foo().then((data) => console.log(data));
 
 
-async function fun() {
-    let p = new Promise((resolve, reject) => {
-        setTimeout(()=>resolve(100), 6000);
-    })
-    let num = await p; 
-    console.log(num);
-}
+// async function fun() {
+//     let p = new Promise((resolve, reject) => {
+//         setTimeout(()=>resolve(100), 6000);
+//     })
+//     let num = await p; //await stops the execution at this line until promise is settled
+//     console.log(num); 
+//     console.log("hello");
+// }
 
-
-fun()
+// fun()
+// for (let index = 0; index < 10; index++) {
+//     console.log(index);
+    
+// }
 
 //promise
-console.log("start");
+// console.log("start");
 
-const promise1 = new Promise((resolve, reject) => {
-  console.log(1);
-  resolve(2);
-});
+// const promise1 = new Promise((resolve, reject) => {
+//   console.log(1);
+//   resolve(2);
+// });
 
-promise1.then((res) => {
-  console.log(res);
-});
+// promise1.then((res) => {
+//   console.log(res);
+// });
 
-console.log("end");
+// console.log("end");
 
 //cb+promise
-console.log("start");
+// console.log("start");
 
-setTimeout(() => {
-  console.log("setTimeout");
-});
+// setTimeout(() => {
+//   console.log("setTimeout");
+// },);
 
-Promise.resolve().then(() => {
-  console.log("resolve");
-});
+// Promise.resolve().then(() => {
+//   console.log("resolve");
+// });
 
-console.log("end");
+// console.log("end");
+
+
+async function getGithubAvatar() {
+    let githubUserRequest =  await fetch(`https://api.github.com/users/goelabhishek694`);
+    let data = await githubUserRequest.json();
+    const img = document.createElement("img");
+    img.src = data.avatar_url;
+    img.style.height = "300px";
+    img.style.width = "300px";
+    img.style.borderRadius = "50%";
+    document.body.append(img);
+    await new Promise((resolve, reject) => {
+        return setTimeout(resolve, 3000);
+    });
+    img.remove();
+    console.log("dp removed");
+    return data
+}
+
+let ans = getGithubAvatar();
+console.log(ans);
+ans.then(data => console.log(data));
+
+// function fun1() {
+//     return
+// }
+// let ans = fun1();
+// console.log(ans);
 
 
 
