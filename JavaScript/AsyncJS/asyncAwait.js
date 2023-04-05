c3cc4da2dc496be6db6af9e6bd885ea5b5b016a7
goelabhishek694 <goelabhishek694@gmail.com>
2023-04-05 21:08:47 +0530

    :fire: microtask queue,callback queue, execution of async in js engine

--- JavaScript/AsyncJS/asyncAwait.js
+++ JavaScript/AsyncJS/asyncAwait.js
@@ -9,11 +9,41 @@
 
 async function fun() {
     let p = new Promise((resolve, reject) => {
-        setTimeout(resolve(100), 6000);
+        setTimeout(()=>resolve(100), 6000);
     })
     let num = await p; 
     console.log(num);
 }
 
+
 fun()
+
+//promise
+console.log("start");
+
+const promise1 = new Promise((resolve, reject) => {
+  console.log(1);
+  resolve(2);
+});
+
+promise1.then((res) => {
+  console.log(res);
+});
+
+console.log("end");
+
+//cb+promise
+console.log("start");
+
+setTimeout(() => {
+  console.log("setTimeout");
+});
+
+Promise.resolve().then(() => {
+  console.log("resolve");
+});
+
+console.log("end");
+
+
 
