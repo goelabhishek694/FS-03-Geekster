146ed4ff5e3c158ba149d28b9d5a4b9a1c8b36f0
goelabhishek694 <goelabhishek694@gmail.com>
2023-04-06 22:08:30 +0530

    :zap: fetch, display emojis, search emoji started

--- JavaScript/emojiSearch/index.js
+++ JavaScript/emojiSearch/index.js
@@ -6,13 +6,34 @@ fetch('emoji.json')
         displayEmojis(allEmojis)
     });
 
-function displayEmojis(emojis) {
+function displayEmojis(emojiObj) {
     //ul slect
     const ul = document.querySelector("#emoji-list");
     ul.innerHTML = '';
-    Object.keys(emojis).forEach(emoji => {
-        const li = document.createElement("li");
-        li.innerHTML = `${emojis[emoji]}`;
-        ul.appendChild(li);
-    })
+    Object.keys(emojiObj).forEach((emojiName) => {
+      const li = document.createElement("li");
+      li.innerHTML = `${emojiObj[emojiName]}`;
+      ul.appendChild(li);
+    });
 }
+
+const emojiInput = document.querySelector("#emoji-input");
+emojiInput.addEventListener('keyup', (e) => {
+    // console.log(emojiInput.value);
+    e.preventDefault();
+    getEmojis(emojiInput.value)
+})
+
+function getEmojis(searchText) {
+    let filteredEmojis = {};
+    Object.keys(allEmojis).forEach(emojiName => {
+        // if (/*emojiName and searchText matches*/) {
+            filteredEmojis[emojiName] = allEmojis[emojiName]
+        // }
+    });
+    displayEmojis(filteredEmojis)
+}
+
+
+/* rfd 
+ ReadableStream*/
