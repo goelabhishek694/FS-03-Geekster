ce1ba2c2f359e2dbeac4c59354e1d322d7d83647
goelabhishek694 <goelabhishek694@gmail.com>
2023-04-13 22:11:22 +0530

    :fire: setTimeout interview Ques

--- JavaScript/AdvancedJS/index.js
+++ JavaScript/AdvancedJS/index.js
@@ -1,4 +1,4 @@
-//lexical env, scope chaining example 
+//lexical env, scope chaining example
 // function a() {
 //     c();
 //     function c() {
@@ -10,7 +10,7 @@
 // console.log(b);
 
 //closures
-// functions along with its lexical scope forms a closure. in other words a closure gives you and access to outer function's scope to inner function . 
+// functions along with its lexical scope forms a closure. in other words a closure gives you and access to outer function's scope to inner function .
 
 // function x() {
 //     var aa=100;
@@ -28,8 +28,8 @@
 // // function y() {
 // //         console.log(a);
 // //     }
-// ans();  
-// ans forms a closure , because it has access to it's local scope + it's parent's scope. 
+// ans();
+// ans forms a closure , because it has access to it's local scope + it's parent's scope.
 
 // function x() {
 //     var a = 7;
@@ -43,16 +43,76 @@
 // console.log(z);
 // z();
 
-function z() {
-    var b = 900;
-    function x(){
-        var a = 7;
-        function y() {
-            console.log(a,b);
-        }
-        y();
-    }
-    x();
+// function z() {
+//     var b = 900;
+//     function x(){
+//         var a = 7;
+//         function y() {
+//             console.log(a,b);
+//         }
+//         y();
+//     }
+//     x();
+// }
+// z();
+
+// function x() {
+//     var i = 1;
+//     setTimeout(function () {
+//         console.log(i);
+//     }, 3000);
+//     console.log("hello");
+// }
+// x();  // hello 1
+
+// for (var i = 1; i <= 5; i++){
+//     setTimeout(function () {
+//         console.log(i);
+//     }, i * 1000);
+// }
+// 5 setTimeout, i = 6;
+
+// for (let i = 1; i <= 5; i++) {
+//   setTimeout(function () {
+//     console.log(i);
+//   }, i * 1000);
+// }
+
+// for (let i = 1; i <= 3; i++) {
+//   setTimeout(function () {
+//     i += 100;
+//     console.log(i);
+//   }, i * 1000);
+// }
+
+for (var i = 1; i <= 5; i++) {
+  setTimeout(function () {
+    i += 100;
+    console.log(i);
+  }, i * 1000);
 }
-z();
+
+// print 1 2 3 4 5 w/0 using let keyword 
+for (var i = 1; i <= 5; i++){
+    setTimeout(function () {
+        console.log(i);
+    }, i * 1000);
+}
+
+//genius sol by vishnu 
+for (var i = 1; i <= 5; i++) {
+  setTimeout(function a() {
+    i += 10;
+    console.log(Math.floor(i / 10));
+  });
+}
+
+//expected soln by krishna
+for (var i = 1; i <= 5; i++) {
+  (function (num) {
+    setTimeout(function () {
+      console.log(num);
+    }, num * 1000);
+  })(i);
+}
 
