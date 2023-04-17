6d75760c948ff6d6252f4768b4effd818891dd2e
goelabhishek694 <goelabhishek694@gmail.com>
2023-04-17 21:22:25 +0530

    :fire: call apply bind finished

---
+++ JavaScript/AdvancedJS/CAB.js
@@ -0,0 +1,59 @@
+//<--------CALL,APPLY,BIND-------->
+
+let player1 = {
+  fName: "Rahul",
+  lName: "Dravid",
+  welcome: function () {
+    console.log(this.fName + " "+this.lName);
+  },
+};
+
+player1.welcome();
+
+let player2 = {
+  fName: "Sachin",
+  lName: "Tendulkar",
+};
+
+//function borrowing
+// player1.welcome() //-> somehow "this" should point to player2 instead of player1
+player1.welcome.call(player2);
+//functionName.call(name of object to whom we want "this" to point to)
+
+let player3 = {
+  fName: "Kapil",
+  lName: "Dev",
+};
+
+// parameters received from call method
+function welcome(email,age) {
+    console.log(`Welcome ${this.fName} ${this.lName}
+    Email ${email}
+    Age ${age}`);
+}
+//          name of object, n number of arguments
+// welcome.call(player3,"kd83@gmail.com",60);
+welcome.call(player2,"masterblaster@gmail.com",50); //this->player2 and welcome fn is called 
+
+//Apply
+let argsArr = ["kd83@gmail.com", 60];
+welcome.apply(player3, argsArr);
+
+
+//bind 
+// as soon as we use call method, the function is called.
+//in bind method "this" keyword gets binded to obj of our choice and returns a function which can be called later on.
+
+let welcomeSachin = welcome.bind(player2, "masterblaster@gmail.com", 55); //this->player2
+console.log(welcomeSachin);
+welcomeSachin(); //-> welcome fn is called
+
+
+welcomeSachin.call(player3); //no effect
+welcomeSachin.bind(player3); // no effect 
+
+//polyfills
+
+
+
+
